6dbb8b70e07dc99499e67a2eacd038ed66db81e5
freeman-lab <the.freeman.lab@gmail.com>
2021-05-26 10:09:54 -0700

    update config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const isDev =
+  process.env.VERCEL_ENV === 'preview' || process.env.NODE_ENV === 'development'
+
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 })
 
+
 module.exports = withMDX({
   pageExtensions: ['jsx', 'js', 'md', 'mdx'],
+  assetPrefix: isDev ? '' : 'https://carbon-removal-application.carbonplan.org',
 })
