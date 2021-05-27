6dd5cc9f81b4c376674d466cc14ed194572fcb54
freeman-lab <the.freeman.lab@gmail.com>
2021-05-27 00:13:50 -0700

    mobile padding

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ const Layout = ({ children, showBack = true }) => {
   return (
     <>
     <Meta description={'General-purpose application for soliciting carbon removal procurement'} title={'carbon removal application / research / carbonplan'} />
-    <Box sx={{ width: '80%', maxWidth: '800px', my: [5], mx: 'auto' }}>
+    <Box sx={{ width: 'calc(100% - 32px)', maxWidth: '800px', my: [5], mx: 'auto' }}>
       {showBack && <NextLink href='/research/carbon-removal-application' passHref><Link sx={{textDecoration: 'none'}}>Back</Link></NextLink>}
       { children }
     </Box>
