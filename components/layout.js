87cadac83182e12efedd97c92a000e62f3e3c409
freeman-lab <the.freeman.lab@gmail.com>
2022-02-10 11:12:53 -0800

    title update

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import { Meta } from '@carbonplan/components'
 const Layout = ({ children, showBack = true }) => {
   return (
     <>
-    <Meta description={'General-purpose application for soliciting carbon removal procurement'} title={'carbon removal application / research / carbonplan'} />
+    <Meta description={'General-purpose application for soliciting carbon removal procurement'} title={'Carbon removal application – CarbonPlan'} />
     <Box sx={{ width: ['calc(100% - 32px)', 'calc(100% - 64px)', 'calc(100% - 128px)'], maxWidth: '800px', my: [4, 5, 5], mx: 'auto' }}>
       {showBack && <NextLink href='/research/carbon-removal-application' passHref><Link sx={{textDecoration: 'none'}}>Back</Link></NextLink>}
       { children }
